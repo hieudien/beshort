b863763455f220277cc8c581073e0fc9723d9f87
HieuDT <hieu@wiserobot.com>
2021-07-04 18:29:41 +0700

    done short link

--- services/beshort.js
+++ services/beshort.js
@@ -2,23 +2,31 @@ import axios from "axios"
 
 const getShortUrl = async function(longUrl) {
     if (!longUrl || !validURL(longUrl)) {
-        return {
-            error: 'Please input a valid URL'
-        }
+        return { error: 'Please input a valid URL' }
     }
     const header = {
-        headers: { Authorization: "Bearer " + process.env.BEARER_TOKEN }
+        headers: { 
+            Authorization: "Bearer " + process.env.NEXT_PUBLIC_BEARER_TOKEN,
+            'Content-Type': 'application/json'
+        }
     }
     const body = {
         "long_url": longUrl
     }
-    console.log(process.env.SHORT_END_POINT);
-    const result = await axios.post(process.env.SHORT_END_POINT, body, header)
-    if (!result) {
-        return {
-            error: 'Server error!'
+    let result = {}
+    await axios.post(process.env.NEXT_PUBLIC_SHORT_END_POINT, body, header)
+    .then(res => {
+        const { data: { link }} = res
+        result = { link }
+    })
+    .catch(error => {
+        result = { error: 'Server error!'}
+        let { response: { data: { message }}} = error
+        if (message){
+            message === 'ALREADY_A_BITLY_LINK' && (message = 'This is already a bittly link.')
+            result = { error: message }
         }
-    }
+    })
     return result
 }
 
